05f52e1e404d7742cd60b85fce5c387c7884e365
ThisWasAryan <aryanrajviiid12@gmail.com>
2026-09-19 18:29:03 +0530

    fix: revert base path to fix local dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/3d_portfolio/',
   plugins: [react()],
   build: {
     rollupOptions: {
